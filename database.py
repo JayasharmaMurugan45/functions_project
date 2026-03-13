661541b14d02600759329f089832edae550ad0c3
Jayasharma <yourgithubemail@gmail.com>
2026-03-13 18:18:44 +0530

    database changes

--- database.py
+++ database.py
@@ -1,15 +1,7 @@
 import mysql.connector
 def connection():
-    con=mysql.connector.connect(host="ballast.proxy.rlwy.net",user="root",password="fyCCgcmVZMbnkXiXSvRRAWIKwUnbBWZr",database="railway",port="38908")
+    con=mysql.connector.connect(host="shortline.proxy.rlwy.net",user="root",password="ykyTiAkFsOCYVWLCVFHOzvlezOlOfdml",database="railway",port="56916")
     # print("jay")
-    cur=con.cursor()
-    query = "ALTER TABLE registar MODIFY id INT NOT NULL AUTO_INCREMENT PRIMARY KEY"
-
-    cur.execute(query)
-
-    con.commit()
-    cur.close()
-    con.close()
     return con
     
 connection()
