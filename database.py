f1d597d6df2053e3b5324d0ceb58ef6e0080449e
Jayasharma <yourgithubemail@gmail.com>
2026-03-24 16:02:42 +0530

    database changes

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import mysql.connector
 def connection():
-    con=mysql.connector.connect(host="shortline.proxy.rlwy.net",user="root",password="ykyTiAkFsOCYVWLCVFHOzvlezOlOfdml",database="railway",port="56916")
+    con=mysql.connector.connect(host="mysql.railway.internal",user="root",password="ykyTiAkFsOCYVWLCVFHOzvlezOlOfdml",database="railway",port="3306")
     # print("jay")
     return con
     
